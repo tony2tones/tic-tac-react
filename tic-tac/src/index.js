afd42bdf825e15ad08d4a79555ab91d87ac9637c
tony2tones <anthony.marque@gmail.com>
2018-01-22 16:08:47 +0200

    adding a handclick to pass a value to check for score

--- tic-tac/src/index.js
+++ tic-tac/src/index.js
@@ -10,18 +10,30 @@ class Square extends React.Component {
         }
     }
 
-render() {
-    return (
-        <button className="square" onClick={() => this.setState({value: 'X'})}>
-            {this.state.value}
-        </button>
-    );
+    render() {
+        return (
+            <button className="square" onClick={() => this.setState({ value: 'X' })}>
+                {this.state.value}
+            </button>
+        );
+    }
 }
-  }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
-        return <Square value={i + 1} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        )
     }
 
     render() {
