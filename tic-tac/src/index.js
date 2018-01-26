a6b4ded158e4f6e1b5cefacb552a71d87368dc11
tony2tones <anthony.marque@gmail.com>
2018-01-26 08:38:36 +0200

    added calculate winner functionality

--- tic-tac/src/index.js
+++ tic-tac/src/index.js
@@ -3,12 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Square(props) {
-        return (
-            <button className="square" onClick={props.onClick}>
-                {props.value}
-            </button>
-        );
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
     }
+    return null;
+}
 
 class Board extends React.Component {
     constructor(props) {
@@ -21,9 +41,12 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i]= this.state.xIsNext ? 'X' : 'O';
+        if(calculateWinner(squares || squares[i])){
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
-            squares:squares,
+            squares: squares,
             xIsNext: !this.state.xIsNext,
         });
     }
@@ -38,7 +61,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if(winner) {
+            status = 'Winner '+ winner;
+        } else {
+            status = 'Next Player ' + (this.xIsNext ? "x" : "O");
+        }
 
         return (
             <div>
